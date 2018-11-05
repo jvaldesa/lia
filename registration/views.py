37e24d1ba3300ca63c4c22909725044b55272bf2
Jvaldes <jvaldes_a@hotmail.com>
2018-11-05 12:34:43 -0600

    Agregamos la app registration

---
+++ registration/views.py
@@ -0,0 +1,23 @@
+from .forms import UserCreationFormEmail
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django import forms
+
+
+# Create your views here.
+class SignUpView(CreateView):
+    form_class = UserCreationFormEmail
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return reverse_lazy('login') + '?register'
+
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        # Modificar en tiempo real
+        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder':'Nombre de ususario'})
+        form.fields['email'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder':'Dirección email'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Repite la Contraseña'})
+        return form
